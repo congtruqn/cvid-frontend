5f11e68254040d40f662c93f048bae942a736deb
tronghoangbk <hoang.nguyentrong197@gmail.com>
2022-04-26 15:19:51 +0700

    #4

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,11 +2,13 @@
     <div id="register">
         <form>
             <h1>Register</h1>
-            <input type="text" v-model="firstname" placeholder="First Name" />
-            <input type="text" v-model="lastname" placeholder="Last Name" />
-            <input type="email" v-model="email" placeholder="Email">
-	        <input type="password"  v-model="password" placeholder="Password">
-            <input type="password"  v-model="password2" placeholder="Confirm Password">
+            
+                <p v-if="errors.length > 0">{{errors[0].msg}}</p>
+                <input type="text" v-model="firstname" placeholder="First Name" /><br/>
+                <input type="text" v-model="lastname" placeholder="Last Name" /><br/>
+                <input type="email" v-model="email" placeholder="Email"><br/>
+                <input type="password"  v-model="password" placeholder="Password"><br/>
+                <input type="password"  v-model="password2" placeholder="Confirm Password"><br/>
 	        <button type="submit" class="btn btn-default" @click="handleSubmit">Register</button>
         </form>
     </div>
@@ -21,27 +23,30 @@
                 email : "",
                 password : "",
                 password2 : "",
+                errors : []
             }
         },
         methods : {
             handleSubmit(e){
                 e.preventDefault()
-                if (this.password.length > 0) {
-                    this.$http.post('api/employee/register', {
-                        firstname: this.firstname,
-                        lastname: this.lastname,
-                        email: this.email,
-                        password: this.password,
-                        password2: this.password2
-                    })
-                    .then(response => {
-                        console.log(response)
+                this.$http.post('api/employee/register', {
+                    firstname: this.firstname,
+                    lastname: this.lastname,
+                    email: this.email,
+                    password: this.password,
+                    password2: this.password2
+                })
+                .then(response => {
+                    if (response.data == 'ok'){
                         this.$router.push('/login')
-                    })
-                    .catch(function (error) {
-                        console.error(error.response);
-                    });
-                }
+                    }
+                    else {
+                        this.errors = response.data;
+                    }
+                })
+                .catch(function (error) {
+                    console.error(error.response);
+                });
             }
         }
     }
