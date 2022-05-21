f703b47429ff8c27ad6432a05c3bbb109165b86a
Nguyễn Trọng Hoàng <75709841+tronghoangbk@users.noreply.github.com>
2022-05-21 11:43:14 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://issue-0-cvid-api-ggczm4ik6q-an.a.run.app"
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://issue-0-cvid-api-ggczm4ik6q-an.a.run.app"
+// export const BASE_URL = "http://localhost:3000"
