61e1644b3bea618c9b03a6edda60e7349c3bdab3
tronghoangbk <hoang.nguyentrong197@gmail.com>
2022-08-16 11:28:56 +0700

    1

--- src/components/business/CandidateList.vue
+++ src/components/business/CandidateList.vue
@@ -3,42 +3,90 @@
     <div class="container">
         <!-- <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">Job Listing</h1> -->
         <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.3s">
-            <div class="row gy-1">
-                <a :href="'/business/cvid/'+index._id" target="_blank" v-for="index in cvid_list" class="job-item p-4 mb-2">
-                    <div class="row">
-                        
-                        <div class="col-sm-12 col-md-7 d-flex align-items-center">
-                            <img class="flex-shrink-0 img-fluid border rounded" src="img/com-logo-1.jpg" alt="" style="width: 80px; height: 80px;">
-                            <div class="text-start ps-4">
-                                <h5 class="mb-3">{{index.name}}</h5>
-                                <span class="text-truncate me-3"><i class="fas fa-file-alt"></i> Điểm CV: {{index.point}}/10</span>
-                                <span class="text-truncate me-3"><i class="fas fa-building"></i> Cấp bậc: {{index.level}}</span>
-                                <span class="text-truncate me-0"><i class="far fa-page"></i>Chuyên nghành: {{index.skill}}</span>
+            <ul class="nav nav-pills d-inline-flex justify-content-center border-bottom mb-5">
+                <li class="nav-item">
+                    <a class="d-flex align-items-center text-start mx-3 ms-0 pb-3 active" data-bs-toggle="pill" href="#tab-1">
+                        <h6 class="mt-n1 mb-0">Chưa thanh toán</h6>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="d-flex align-items-center text-start mx-3 pb-3" data-bs-toggle="pill" href="#tab-2">
+                        <h6 class="mt-n1 mb-0">Đã thanh toán</h6>
+                    </a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="d-flex align-items-center text-start mx-3 me-0 pb-3" data-bs-toggle="pill" href="#tab-3">
+                        <h6 class="mt-n1 mb-0">Part Time</h6>
+                    </a>
+                </li> -->
+            </ul>
+            <div class="tab-content">
+                <div id="tab-1" class="tab-pane fade show p-0 active">
+                <div class="row gy-1">
+                    <a :href="'/business/cvid/'+index._id" target="_blank" v-for="index in cvid_list" class="job-item p-4 mb-2">
+                        <div class="row">
+                            
+                            <div class="col-sm-12 col-md-7 d-flex align-items-center">
+                                <img class="flex-shrink-0 img-fluid border rounded" src="img/com-logo-1.jpg" alt="" style="width: 80px; height: 80px;">
+                                <div class="text-start ps-4">
+                                    <h5 class="mb-3">{{index.name}}</h5>
+                                    <span class="text-truncate me-3"><i class="fas fa-file-alt"></i> Điểm CV: {{index.point}}/10</span>
+                                    <span class="text-truncate me-3"><i class="fas fa-building"></i> Cấp bậc: {{index.level}}</span>
+                                    <span class="text-truncate me-0"><i class="far fa-page"></i>Chuyên nghành: {{index.skill}}</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-11 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
+                                <div class="d-flex mb-3">
+                                    <!-- <a class="btn btn-light btn-square me-3" href=""><i class="far fa-heart text-primary"></i></a> -->
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ScheduleModal" @click.prevent="employee_id = index._id" v-for="item in job_list" v-if="item.employee_id == index._id && !item.schedule">Đặt lịch phỏng vấn</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#ScheduleModal" @click.prevent="employee_id = index._id" v-for="item in job_list" v-if="item.employee_id == index._id && item.schedule">Thay đổi lịch phỏng vấn</button>
+                                </div>
+                                <span class="text-truncate">100.000 VNĐ</span>
+                                <small class="text-truncate" v-for="item in job_list" v-if="item.employee_id == index._id"><i class="far fa-calendar-alt text-primary me-2"></i>Lịch phỏng vấn: {{item.schedule?item.schedule.replace('T', ' '):'Chưa có'}}</small>
+                            </div>
+                            <div class="form-check col-sm-1 col-md-1 d-flex align-items-center justify-content-center">
+                                <input type="checkbox" class="form-check-input" v-model="selected" :value="index._id" 
+                                v-for="item in job_list" v-if="item.employee_id == index._id && item.schedule"/>
                             </div>
                         </div>
-                        <div class="col-sm-11 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
-                            <div class="d-flex mb-3">
-                                <!-- <a class="btn btn-light btn-square me-3" href=""><i class="far fa-heart text-primary"></i></a> -->
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ScheduleModal" @click.prevent="employee_id = index._id">Đặt lịch</button>
-                                
-                            </div>
-                            <span class="text-truncate">100.000 VNĐ</span>
-                            <small class="text-truncate" v-for="item in job_list" v-if="item.employee_id == index._id"><i class="far fa-calendar-alt text-primary me-2"></i>Lịch phỏng vấn: {{item.schedule?item.schedule.replace('T', ' '):'Chưa có'}}</small>
-                        </div>
-                        <div class="form-check col-sm-1 col-md-1 d-flex align-items-center justify-content-center">
-                            <input type="checkbox" class="form-check-input" v-model="selected" :value="index._id" 
-                            v-for="item in job_list" v-if="item.employee_id == index._id && item.schedule"/>
-                        </div>
-                    </div>
-                </a>   
-            </div>
-            <div class="mt-2 d-flex justify-content-end">
-                <div class="d-flex mb-3">
-                    <a class="p-1 fs-4 me-2"><i class="fw-bold">Tổng:</i> {{calculator.toString().replace(/\B(?=(\d{3})+(?!\d))/g,".")}} VNĐ</a>
-                    <a class="btn btn-primary" @click="pay">Thanh toán</a>
+                    </a>   
                 </div>
-                <!-- <small class="text-truncate"><i class="far fa-calendar-alt text-primary me-2"></i>Lịch phỏng vấn: Chưa có</small> -->
+                <div class="mt-2 d-flex justify-content-end">
+                    <div class="d-flex mb-3">
+                        <a class="p-1 fs-4 me-2"><i class="fw-bold">Tổng:</i> {{calculator.toString().replace(/\B(?=(\d{3})+(?!\d))/g,".")}} VNĐ</a>
+                        <a class="btn btn-primary" @click="pay">Thanh toán</a>
+                    </div>
+                    <!-- <small class="text-truncate"><i class="far fa-calendar-alt text-primary me-2"></i>Lịch phỏng vấn: Chưa có</small> -->
+                </div>
+                </div>
+                <div id="tab-2" class="tab-pane fade show p-0">
+                <div class="row gy-1">
+                    <a :href="'/business/cvid/'+index._id" target="_blank" v-for="index in cvid_list_paid" class="job-item p-4 mb-2">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-7 d-flex align-items-center">
+                                <img class="flex-shrink-0 img-fluid border rounded" src="img/com-logo-1.jpg" alt="" style="width: 80px; height: 80px;">
+                                <div class="text-start ps-4">
+                                    <h5 class="mb-3">{{index.name}}</h5>
+                                    <span class="text-truncate me-3"><i class="fas fa-file-alt"></i> Điểm CV: {{index.point}}/10</span>
+                                    <span class="text-truncate me-3"><i class="fas fa-building"></i> Cấp bậc: {{index.level}}</span>
+                                    <span class="text-truncate me-0"><i class="far fa-page"></i>Chuyên nghành: {{index.skill}}</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
+                                <div class="d-flex mb-3">
+                                    <!-- <a class="btn btn-light btn-square me-3" href=""><i class="far fa-heart text-primary"></i></a> -->
+                                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#ScheduleModal" @click.prevent="employee_id = index._id">Thay đổi lịch phỏng vấn</button>
+                                    
+                                </div>
+                                
+                                <small class="text-truncate" v-for="item in job_list_paid" v-if="item.employee_id == index._id"><i class="far fa-calendar-alt text-primary me-2"></i>Lịch phỏng vấn: {{item.schedule?item.schedule.replace('T', ' '):'Chưa có'}}</small>
+                            </div>
+                        </div>
+                    </a>   
+                </div>
+                </div>
             </div>
+            
         </div>
         <div class="modal fade" id="ScheduleModal" tabindex="-1" aria-labelledby="ScheduleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -70,7 +118,9 @@ const {BASE_URL} =  require('../../utils/config')
             return {
                 employee_id: '',
                 cvid_list: [],
+                cvid_list_paid: [],
                 job_list: [],
+                job_list_paid: [],
                 selected: [],
                 schedule: '',
             }
@@ -139,12 +189,22 @@ const {BASE_URL} =  require('../../utils/config')
                 res.data.forEach(job =>{
                     if (job.type == 2 && job.status == 0){
                         this.job_list.push(job)
+                    } else if (job.type == 2 && job.status == 1){
+                        this.job_list_paid.push(job)
                     }
                 })
                 this.$http.post(`${BASE_URL}/employee/list/cvid`, {
                     selected: this.job_list.map((obj) => obj.employee_id)
                 }).then(res => {
                     this.cvid_list = res.data
+                 
+                }).catch(err => {
+                    console.log(err)
+                })
+                this.$http.post(`${BASE_URL}/employee/list/cvid`, {
+                    selected: this.job_list_paid.map((obj) => obj.employee_id)
+                }).then(res => {
+                    this.cvid_list_paid = res.data
                  
                 }).catch(err => {
                     console.log(err)
