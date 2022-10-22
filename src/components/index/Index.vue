67f64751ad0a19b2c867e0d168d5302c88bfba22
Nguyen Trong Hoang <tronghoang1907@gmail.com>
2022-10-22 09:02:45 +0700

    1

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,8 +1,26 @@
 <template>
-    <div class="position-absolute top-50 start-50 translate-middle">
-        <div class="d-grid gap-2 mx-auto">
-            <a href="/employee/login" class="btn btn-primary" >Người tìm việc</a>
-            <a href="/business/login" class="btn btn-primary">Nhà tuyển dụng</a>
+
+    <div class="container py-5" style="height:100vh">
+        <h2 class="p-4 text-center">CHÀO MỪNG ĐẾN VỚI ỨNG DỤNG TÌM KIẾM VIỆC LÀM VÀ TÌM ỨNG VIÊN CHẤT LƯỢNG CAO CVIDPRO</h2>
+      <div class="row justify-content-center">
+        <div class="col-4 text-center">
+          <a
+          href="/employee/login"
+            class="btn btn-primary rounded-circle py-5"
+            style="width: 130px; height: 130px"
+          >
+            Người tìm việc
+          </a>
         </div>
+        <div class="col-4 text-center">
+          <a
+          href="/business/login"
+            class="btn btn-primary rounded-circle py-5"
+            style="width: 130px; height: 130px"
+          >
+            Nhà tuyển dụng
+          </a>
+        </div>
+      </div>
     </div>
 </template>
